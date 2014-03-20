5604a810073b01cc8fe09ef5e9046e714e4782ba
Todd Sukolsky <sukolsky@bu.edu>
2014-03-20 09:28:36 -0400

    first commit

---
+++ Debug/main.d
@@ -0,0 +1,6 @@
+main.d: ../main.cpp ../../PlatformUtils/atmega168A.h \
+ ../../PlatformUtils/stdtypes.h
+
+../../PlatformUtils/atmega168A.h:
+
+../../PlatformUtils/stdtypes.h:
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <avr/io.h>
+#include "../PlatformUtils/atmega168A.h"
+#include "../PlatformUtils/stdtypes.h"
+
+int main()
+{
+	return 0;
+}
